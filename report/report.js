a3e4390aea8e75c65513c20416f2ecd35ffcf8a9
Zubair94 <kazizubair85@yahoo.com>
2018-08-12 13:54:18 +0600

    some changes

--- report/report.js
+++ report/report.js
@@ -1,7 +1,7 @@
 
 const { remote } = require('electron');
 
-var pdf = new jsPDF('p', 'pt', 'a4');
+//var pdf = new jsPDF('p', 'pt', 'a4');
 const database_helper = require('../db_helper');
 var database = database_helper.connect();
 
@@ -48,16 +48,11 @@ depositReportButton.addEventListener('click', (event) => {
             html += "</tbody>"
             html += "</table>"
             document.getElementById("report-deposit").innerHTML = html;
-            margins = {
-                top: 10,
-                bottom: 10,
-                left: 10,
-                right: 10
-              };
-            pdf.addHTML( document.getElementById("report-deposit"), function() {
+            Export2Doc('report-deposit','deposit');
+            /*pdf.addHTML( document.getElementById("report-deposit"), function() {
                 pdf.setFontSize(12);
                 pdf.save('deposit.pdf');
-            });
+            });*/
             
         });
     }
@@ -109,9 +104,10 @@ withdrawReportButton.addEventListener('click', (event) => {
         html += "</tbody>"
         html += "</table>"
         document.getElementById("report-withdraw").innerHTML = html;
-        pdf.addHTML( document.getElementById("report-withdraw"), function() {
+        Export2Doc('report-withdraw','withdraw');
+        /*pdf.addHTML( document.getElementById("report-withdraw"), function() {
             pdf.save('withdraw.pdf');
-        });
+        });*/
     });
         
         
@@ -125,9 +121,39 @@ withdrawReportButton.addEventListener('click', (event) => {
     
 });
 
-function getWithdrawReport(from, to){
-   return new Promise((resolve, reject) => {
+function Export2Doc(element, filename = ''){
+    var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+    var postHtml = "</body></html>";
+    var html = preHtml+document.getElementById(element).innerHTML+postHtml;
+
+    var blob = new Blob(['\ufeff', html], {
+        type: 'application/msword'
+    });
     
-   });
+    // Specify link url
+    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+    // Specify file name
+    filename = filename?filename+'.doc':'document.doc';
+    
+    // Create download link element
+    var downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob ){
+        navigator.msSaveOrOpenBlob(blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = url;
+        
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
+    
+    document.body.removeChild(downloadLink);
 }
 
